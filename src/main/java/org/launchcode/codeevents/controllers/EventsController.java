c76d76fade6aaa57761bbc0a437562d3402cd435
Michal Richter <michalrichter@Michals-Air.attlocal.net>
2021-02-21 15:51:01 -0600

    finished exercises. need to work on address and img columns

--- src/main/java/org/launchcode/codeevents/controllers/EventsController.java
+++ src/main/java/org/launchcode/codeevents/controllers/EventsController.java
@@ -8,16 +8,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventsController {
 
-    private static List<String> events = new ArrayList<>();
+//    private static List<String> events = new ArrayList<>();
+
+//    @GetMapping()
+//    public String displayEventsList(Model model) {
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
 
     @GetMapping()
-    public String displayEventsList(Model model) {
+    public String displayEventsHashMap(Model model) {
+        HashMap<String, String> events = new HashMap<>();
+        events.put("AIGA Northwest Arkansas", "AIGA NWA works to advance design as a professional craft, strategic advantage, and vital cultural force here in Northwest Arkansas.");
+        events.put("fayetteville.js", "A monthly group for anyone interested in the JavaScript or software development for the web platform. All skill levels are welcome.");
+        events.put("Bentonville Java Users Group", "A monthly group for anyone interested in the JavaScript or software development for the web platform. All skill levels are welcome.");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -27,9 +38,9 @@ public class EventsController {
         return "events/create";
     }
 
-    @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
-        return "redirect:";
-    }
+//    @PostMapping("create")
+//    public String createEvent(@RequestParam String eventName) {
+//        events.add(eventName);
+//        return "redirect:";
+//    }
 }
