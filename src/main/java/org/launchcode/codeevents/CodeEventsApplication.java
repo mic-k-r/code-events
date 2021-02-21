f9a0ef7ec22e111f66e8d6c228dc087a21a60926
Michal Richter <michalrichter@Michals-Air.attlocal.net>
2021-02-21 12:40:03 -0600

    first commit

---
+++ src/main/java/org/launchcode/codeevents/CodeEventsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.codeevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/codeevents/CodeEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.codeevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
