0f97640b59bcac4945283621879b1cb660f41c22
mic-k-r <69399528+mic-k-r@users.noreply.github.com>
2021-03-03 16:21:36 -0600

    added validation and error messages to form fields. still working on isRegistered property and form field.

--- src/main/java/org/launchcode/codeevents/controllers/EventsController.java
+++ src/main/java/org/launchcode/codeevents/controllers/EventsController.java
@@ -4,8 +4,10 @@ import org.launchcode.codeevents.data.EventData;
 import org.launchcode.codeevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,12 +23,19 @@ public class EventsController {
 
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
+//        model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model) {
+        if(errors.hasErrors()) {
+//            model.addAttribute("Create Event");
+            return "events/create";
+        }
+
         EventData.addEvent(newEvent);
         return "redirect:";
     }
@@ -56,10 +65,10 @@ public class EventsController {
     }
 
     @PostMapping("edit")
-    public String processEditForm(int eventId, String name, String description) {
-        Event event = EventData.getById(eventId);
-        event.setName(name);
-        event.setDescription(description);
+    public String processEditForm(int eventId, @ModelAttribute Event newEvent) {
+        Event existingEvent = EventData.getById(eventId);
+        existingEvent.setName(newEvent.getName());
+        existingEvent.setDescription(newEvent.getDescription());
         return "redirect:";
     }
 }
--- src/main/java/org/launchcode/codeevents/models/Event.java
+++ src/main/java/org/launchcode/codeevents/models/Event.java
@@ -1,17 +1,45 @@
 package org.launchcode.codeevents.models;
 
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 public class Event {
 
     private int id;
     private static int nextId = 1;
+
+    @NotBlank(message="Name is required.")
+    @Size(min=3, max=50, message="Name must be between 3 and 50 characters.")
     private String name;
+
+    @Size(max=500, message="Description too long.")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message="Email is required.")
+    @Email(message="Invalid email. Try again.")
+    private String contactEmail;
+
+    @NotBlank(message="Location is required.")
+    @Size(max= 100, message="Too many characters.")
+    private String location;
+
+    @Min(1)
+    private int numAttendees;
+
+    @AssertTrue
+    private boolean isRegistered = true;
+
+    public Event(String name, String description, String location, int numAttendees, boolean isRegistered, String contactEmail) {
+        this();
         this.name = name;
         this.description = description;
+        this.location = location;
+        this.numAttendees = numAttendees;
+        this.isRegistered = isRegistered;
+        this.contactEmail = contactEmail;
+    }
+
+    public Event() {
         this.id = nextId;
         nextId++;
     }
@@ -34,6 +62,38 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getNumAttendees() {
+        return numAttendees;
+    }
+
+    public void setNumAttendees(int numAttendees) {
+        this.numAttendees = numAttendees;
+    }
+
+    public boolean isRegistered() {
+        return isRegistered;
+    }
+
+    public void setRegistered(boolean registered) {
+        isRegistered = registered;
     }
 
     @Override
